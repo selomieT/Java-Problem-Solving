0562cacbc5739efb9b77bc9ff60821f122e9a7e3
selomieT <selomie.mteshome@gmail.com>
2019-09-21 11:26:57 -0600

    palindrome number method added

--- src/main/java/com/st/problemSolving/Palindrome.java
+++ src/main/java/com/st/problemSolving/Palindrome.java
@@ -6,6 +6,12 @@ public class Palindrome {
         System.out.println("is Bob palindrom "+isPalindrome("Bob"));
         System.out.println("is Wow palindrom "+isPalindrome("Wow"));
         System.out.println("is Seli palindrom "+isPalindrome("seli"));
+
+        System.out.println("is number 232 palindrom "+numberPalindrom(232));
+
+        System.out.println("is number 400 palindrom "+numberPalindrom(400));
+
+        System.out.println("is number 444 palindrom "+numberPalindrom(444));
     }
 
     public static boolean isPalindrome(String str){
@@ -17,5 +23,16 @@ public class Palindrome {
     public static String reverseString(String input){
         if(input==null ||input.length()==0)return input;
         return reverseString(input.substring(1))+input.charAt(0);
+    }
+
+    public static boolean numberPalindrom(int n){
+        int palindrom=n;
+        int reverse=0;
+        while(palindrom!=0){
+            reverse=reverse*10+palindrom%10;
+            palindrom=palindrom/10;
+        }
+        if(reverse==n)return true;
+        return false;
     }
 }
