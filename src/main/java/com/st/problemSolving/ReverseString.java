09b354c8041fa68aafe7d60f200bcd35a7031298
selomieT <selomie.mteshome@gmail.com>
2019-09-21 10:37:35 -0600

    String reverse using StringBuilder added

--- src/main/java/com/st/problemSolving/ReverseString.java
+++ src/main/java/com/st/problemSolving/ReverseString.java
@@ -5,6 +5,7 @@ public class ReverseString {
 
     public static void main(String[]args){
         System.out.println("String before reversed : seli "+"after reversed : "+reverseString1("seli"));
+        System.out.println("String before reversed : seli "+"after reversed : "+reverseString2("seli"));
     }
 
     public static String reverseString1(String str){
@@ -12,6 +13,14 @@ public class ReverseString {
             return str;
         }
         return reverseString1(str.substring(1))+str.charAt(0);
+
+    }
+    public static String reverseString2(String str){
+        StringBuilder str1=new StringBuilder();
+        for(int i=str.length()-1;i>=0;i--){
+            str1.append(str.charAt(i));
+        }
+        return str1.toString();
 
     }
 }
